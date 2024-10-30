73c691b6c9c8a925b99c11d702da925eb0353ad3
[j chun] <[justinchun224@gmail.com]>
2024-10-30 11:15:16 -0700

    implementing push notifications; added listeners to root layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,24 @@
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform, Alert } from 'react-native';
 import { useFonts } from "expo-font";
-// import "react-native-url-polyfill/auto";
 import { SplashScreen, Stack } from "expo-router";
 import { app } from '../firebase/config';
-
+import Constants from 'expo-constants';
 import GlobalProvider from "../context/GlobalProvider";
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+const BACKGROUND_NOTIFICATION_TASK = 'BACKGROUND-NOTIFICATION-TASK';
+
+TaskManager.defineTask(BACKGROUND_NOTIFICATION_TASK, ({ data: { notification }, error }) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('Received a notification in the background:', notification);
+  // Optionally handle notification data here, like scheduling local notifications
+});
+
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
@@ -23,19 +35,88 @@ const RootLayout = () => {
   });
 
   useEffect(() => {
-    if (error) throw error;
-
     if (fontsLoaded) {
       SplashScreen.hideAsync();
     }
-  }, [fontsLoaded, error]);
+  }, [fontsLoaded]);
 
   if (!fontsLoaded) {
     return null;
+    // Return a splash screen or loader
   }
 
   if (!fontsLoaded && !error) {
     return null;
+    // Return a splash screen or loader
+  }
+
+  useEffect(() => {
+    const setupNotifications = async () => {
+      const expoPushToken = await registerForPushNotificationsAsync();
+      console.log("Expo Push Token:", expoPushToken);
+      // Add function to update push token field in users collection
+
+      Notifications.registerTaskAsync(BACKGROUND_NOTIFICATION_TASK);
+
+      const notificationListener = Notifications.addNotificationReceivedListener(notification => {
+        console.log("Notification Received:", notification);
+      });
+
+      const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
+        console.log("Notification Clicked:", response);
+        // Add function to handle the response (open app to specific target screen)
+      });
+
+      return () => {
+        Notifications.removeNotificationSubscription(notificationListener);
+        Notifications.removeNotificationSubscription(responseListener);
+      };
+    }
+
+    setupNotifications();
+  }, []);
+
+  const handleRegistrationError = (errorMessage) => {
+    Alert.alert(errorMessage);
+    throw new Error(errorMessage);
+  }
+
+  const registerForPushNotificationsAsync = async () => {
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+
+    if (Device.isDevice) {
+      const existingStatus = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const status = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        handleRegistrationError('Permission not granted to get push token for push notification!');
+        return;
+      }
+      // Need to define projectID
+      const projectID = Constants?.expoConfig?.extra?.eas?.projectID ?? Constants?.easConfig?.projectID;
+      if (!projectID) {
+        handleRegistrationError('Project ID not found');
+      }
+      try {
+        const pushTokenString = (await Notifications.getExpoPushTokenAsync(projectID)).data;
+        console.log(pushTokenString);
+        return pushTokenString;
+      } catch (error) {
+        handleRegistrationError(`${error}`);
+      }
+    } else {
+      handleRegistrationError('Must use physical device for push notifications');
+    }
   }
 
   return (
