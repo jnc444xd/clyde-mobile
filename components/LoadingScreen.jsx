aa32d7e632e50f73427f45b8198076b37e2a24c7
[j chun] <[justinchun224@gmail.com]>
2024-11-01 16:23:06 -0700

    loading screen animation fix

--- components/LoadingScreen.jsx
+++ components/LoadingScreen.jsx
@@ -1,15 +1,53 @@
-import { View, Image } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, Animated } from 'react-native';
+import { images } from '../constants';
 
 const LoadingScreen = () => {
+    const scaleAnim = useRef(new Animated.Value(1)).current;
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(scaleAnim, {
+                    toValue: 1.5,
+                    duration: 500,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(scaleAnim, {
+                    toValue: 1,
+                    duration: 500,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(scaleAnim, {
+                    toValue: 1.5,
+                    duration: 500,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(scaleAnim, {
+                    toValue: 1,
+                    duration: 500,
+                    useNativeDriver: true,
+                })
+            ]),
+            {
+                iterations: 1
+            }
+        ).start();
+    }, []);
+
     return (
-        <View className="flex-1 w-full h-full items-center justify-center bg-[#151521]">
-            <Image
-                source={require("../assets/images/clyde-pop.gif")}
-                className="w-64 h-64"
+        <View className="flex-1 items-center justify-center bg-[#151521]">
+            <Animated.Image
+                source={images.logoNoText}
+                style={{
+                    width: 150,
+                    height: 150,
+                    transform: [{ scale: scaleAnim }]
+                }}
                 resizeMode="contain"
             />
         </View>
-    )
+    );
 };
 
 export default LoadingScreen;
